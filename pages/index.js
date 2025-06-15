628157dd61abc53698d95b34984390a8e9f28426
Nathalyml24 <nathalymonsalv@gmail.com>
2025-06-15 02:00:01 -0500

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -17,7 +17,7 @@ export default function Home() {
           </p>
           <div className="flex justify-center gap-4 mt-4">
             <a
-              href="/NathalyMonsalve_CV_QA_SoftwareTester.pdf"
+              href="https://github.com/Nathalyml24/Portafolio-Nath-QA/NathalyMonsalve_CV_QA_SoftwareTester"
               download
               className="px-6 py-2 bg-red-600 text-white rounded-full hover:bg-red-700 transition"
             >
